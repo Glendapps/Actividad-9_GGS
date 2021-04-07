fee2c3ee005324cbbeee757e2a2f892e7324e070
Glendapps <78885940+Glendapps@users.noreply.github.com>
2021-04-07 10:53:29 -0500

    Add files via upload

---
+++ Actividad 9_GGS/Actividad 9_GGS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Actividad 9_GGS
+//
+//  Created by user185903 on 4/7/21.
+//  Copyright © 2021 user185903. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
